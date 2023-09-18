19fa762669280dde64c765cb8351c6f0367a44b5
Emmanuel <emrighi.jba@gmail.com>
2023-09-17 22:52:51 -0300

    saque los botones del home

--- js/home.js
+++ js/home.js
@@ -1,45 +1,17 @@
 
 window.onload = () => {
     // Variables
-    const IMAGES = [ 
-         '../img/bebidas-refrescantes.jpg',
-        '../img/bebidas-refrescantes.jpg',
-        '../img/bebidas-refrescantes.jpg'
-    ];
-    let actualPosition = 0;
-    const buttonBack = document.querySelector('#back');
-    const buttonNext = document.querySelector('#next');
+    const IMAGES = ['../img/bebidas-refrescantes.jpg'];
     const image = document.querySelector('#image');
   
-    function nextPhoto() {
-        if(actualPosition >= IMAGES.length - 1) {
-            actualPosition = 0;
-        } else {
-            actualPosition++;
-        }
-        renderImage();
-    }
-
-    function backPhoto() {
-        if(actualPosition <= 0) {
-            actualPosition = IMAGES.length - 1;
-        } else {
-            actualPosition--;
-        }
-        renderImage();
-    }
-
     function renderImage() {
-        image.style.backgroundImage = `url(${IMAGES[actualPosition]})`;
+        image.style.backgroundImage = `url(${IMAGES[0]})`;
     }
 
-    buttonNext.addEventListener('click', nextPhoto);
-    buttonBack.addEventListener('click', backPhoto);
     document.getElementById("btn-search").addEventListener('click', () => {
         location.href ="../../search.html";
         const name = document.getElementById("input-search").value;      
-        renderMerchandise("", name, "asc");
-        console.log("aaaaa")
+        localStorage.setItem("search", name);
     });
     renderImage();
 } 
