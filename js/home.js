470c5fa063e9cd39c52743689159f29fbf9028e0
hernan9011 <pipivr72@gmail.com>
2023-09-26 22:53:48 -0300

    fix css

--- js/home.js
+++ js/home.js
@@ -4,12 +4,10 @@ async function renderRandomMerchandise() {
     try {
         const API_URL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php';
         const response = await fetch(`${API_URL}`, { method: 'GET' });
-        const midiv = $('#bebidaRandom');
-
         if (!response.ok) {
             throw new Error('Error obtaining Merchandise');
         }
-
+        const midiv = $('#bebidaRandom');
         const RandomMerchandise = await response.json();
         const trago = RandomMerchandise.drinks[0];
         midiv.prepend(letter_randomMerchandise(trago));
@@ -20,3 +18,18 @@ async function renderRandomMerchandise() {
 }
 
 renderRandomMerchandise();
+
+document.getElementById("btn-search").addEventListener('click', () => {
+    location.href = "../../search.html";
+    const name = document.getElementById("input-search").value;
+    localStorage.setItem("search", name);
+});
+
+
+document.getElementById("input-search").addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        location.href = "../../search.html";
+        const name = document.getElementById("input-search").value;
+        localStorage.setItem("search", name);
+    }
+});
